951a1162b8361e246a317c0b025a8ee58dea6732
Larissa Teixeira <lteixeira@4bio.com.br>
2023-08-03 11:48:35 -0300

    Add project files.

---
+++ GraphQL.HotChocolate.API/Startup.cs
@@ -0,0 +1,18 @@
+﻿namespace GraphQL.HotChocolate.API
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env) 
+        { 
+            if(env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+        }
+    }
+}
