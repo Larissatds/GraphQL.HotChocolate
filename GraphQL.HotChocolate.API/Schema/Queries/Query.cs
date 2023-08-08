744e1d7ed776839250b19652b6fe283aa21739c0
Larissa Teixeira <lteixeira@4bio.com.br>
2023-08-08 17:19:40 -0300

    Implementation GraphQL cursor-based and offset-based pagination and add some optimizations to map paginated GraphQL queries

--- GraphQL.HotChocolate.API/Schema/Queries/Query.cs
+++ GraphQL.HotChocolate.API/Schema/Queries/Query.cs
@@ -1,4 +1,5 @@
 ﻿using GraphQL.HotChocolate.API.DTOs;
+using GraphQL.HotChocolate.API.Services;
 using GraphQL.HotChocolate.API.Services.Courses;
 
 namespace GraphQL.HotChocolate.API.Schema.Queries
@@ -13,6 +14,33 @@ namespace GraphQL.HotChocolate.API.Schema.Queries
         }
 
         public async Task<IEnumerable<CourseType>> GetCourses()
+        {
+            IEnumerable<CourseDTO> courseDTOs = await _coursesRepository.GetAll();
+
+            return courseDTOs.Select(c => new CourseType()
+            {
+                Id = c.Id,
+                Name = c.Name,
+                Subject = c.Subject,
+                InstructorId = c.InstructorId
+            });
+        }
+
+        [UseDbContext(typeof(SchoolDbContext))]
+        [UsePaging(IncludeTotalCount = true, DefaultPageSize = 10)]
+        public IQueryable<CourseType> GetPaginatedCourses([ScopedService] SchoolDbContext context)
+        {
+            return context.Courses.Select(c => new CourseType()
+            {
+                Id = c.Id,
+                Name = c.Name,
+                Subject = c.Subject,
+                InstructorId = c.InstructorId
+            });
+        }
+
+        [UseOffsetPaging(IncludeTotalCount = true, DefaultPageSize = 10)]
+        public async Task<IEnumerable<CourseType>> GetOffsetCourses()
         {
             IEnumerable<CourseDTO> courseDTOs = await _coursesRepository.GetAll();
 
